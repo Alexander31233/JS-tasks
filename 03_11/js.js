aeab89ef93c1d32c90a4e2893330f66a365c34e1
Alexander <sanektop10@gmail.com>
2022-11-03 17:21:28 +0600

    feat: 03_11

---
+++ 03_11/js.js
@@ -0,0 +1,21 @@
+
+/*
+let images = document.getElementsByName("img");
+
+images.onclick = alert("its user");
+*/
+
+/*
+
+let links = document.querySelectorAll("a");
+
+links.onmouseenter = alert("Nowhere");
+
+
+*/
+
+function func (x , y) {
+    alert(arguments[0] + " " + arguments[1]);
+}
+
+func("something " , "something"); // не работает со стрелочным массивом
