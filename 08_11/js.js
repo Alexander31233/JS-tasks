d018dc9df493b4f279c081445ec58bc73e78a97c
Alexander <sanektop10@gmail.com>
2022-11-08 17:30:10 +0600

    feat: 08_11

---
+++ 08_11/js.js
@@ -0,0 +1,17 @@
+/*
+1) Дан ul, в нем несколько li. Под ul сделайте кнопку, по нажатию на которую
+в конец ul будет добавляться новый li с текстом 'пункт'*/
+const btn  = document.querySelector('#btn');
+const ul = document.querySelector('#ul');
+
+const newLi = (txt) =>{
+    let li = document.createElement("li")
+    li.innerText = txt.text;
+    ul.append(li);
+}
+btn.addEventListener('click', () =>{
+    newLi({
+        text :"Пункт"
+    })
+
+});
